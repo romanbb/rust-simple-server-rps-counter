0cb92d1b59262c52edaea07c16cfb2f6027c4153
Roman Birg <romanbirg@gmail.com>
2022-08-12 15:46:30 -0700

    working fly.io config and cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,19 @@
-From  rust:1.62.1 as builder
+From  rust:latest as builder
 
 WORKDIR /usr/src/server
+
+COPY Cargo.toml Cargo.lock ./
+RUN mkdir src && echo "fn main(){}" > src/main.rs
+RUN --mount=type=cache,target=/usr/local/cargo/registry \
+    --mount=type=cache,target=/usr/src/server/target \
+    cargo build --release
+
 COPY . .
 
 RUN cargo install --path .
 
+
 FROM debian:buster-slim
 COPY --from=builder /usr/local/cargo/bin/main /usr/local/bin/main
-CMD ["main"]
+
+EXPOSE 8080
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,18 @@
 use server::ThreadPool;
 use std::fmt::Display;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, SystemTime};
+
+const SNAPSHOT_WINDOW_SIZE: usize = 10;
 
 struct Snapshot {
     total_request_count: u128,
     elapsed_time_millis: u128,
 }
+
 impl Display for Snapshot {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -20,6 +22,7 @@ impl Display for Snapshot {
         )
     }
 }
+
 fn get_rps_for_snapshots(older: &Snapshot, newer: &Snapshot) -> f64 {
     if newer.total_request_count == older.total_request_count {
         return 0.0;
@@ -30,7 +33,9 @@ fn get_rps_for_snapshots(older: &Snapshot, newer: &Snapshot) -> f64 {
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], 8080));
+    let listener = TcpListener::bind(addr).expect("Issue binding to IP Address");
+    // listener.set_ttl(100).expect("Couldn't set TTL");
     let request_counter = Arc::new(Mutex::new(0));
     let snapshots: Arc<Mutex<Vec<Snapshot>>> = Arc::new(Mutex::new(Vec::new()));
     let start = SystemTime::now();
@@ -60,44 +65,28 @@ fn main() {
                     let mut snapshots_stats = snapshots_for_stats_thread.lock().unwrap();
                     let current_requests = rps_counter.lock().unwrap();
 
-                    // for i in 1..snapshots_stats.len() {
-                    //     let rps_slice = get_rps_for_snapshots(snapshots_stats.get(i-1).unwrap(), snapshots_stats.get(i).unwrap());
-                    //     println!("RPS slice is {}", rps_slice);
-                    // }
-
                     let elapsed_ms = start.elapsed().ok().unwrap().as_millis();
+
+                    // need 2 to compare
                     if snapshots_stats.len() > 2 {
                         let rps = get_rps_for_snapshots(
                             snapshots_stats.get(0).unwrap(),
                             snapshots_stats.get(snapshots_stats.len() - 1).unwrap(),
                         );
 
-                        // let rps = *current_requests / elapsed_ms;
                         println!("RPS {}, total requests: {}", rps, *current_requests);
                     }
+
+                    // save snapshot
                     snapshots_stats.push(Snapshot {
                         elapsed_time_millis: elapsed_ms,
                         total_request_count: *current_requests,
                     });
 
-                    const SNAPSHOT_WINDOW_SIZE: usize = 10;
-
-                    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
-                    // trim to 10
-                    // if (arr.len < SNAPSHOT_WINDOW_SIZE)  do nothing
-                    // else len - SNAPSHOT_WINDOW_SIZE = 14-10 = 4
-                    // remove 0-4
-
+                    // trim the beginning of the array to retain N snapshots
                     let upper_bound_remove = snapshots_stats.len()
                         - std::cmp::min(snapshots_stats.len(), SNAPSHOT_WINDOW_SIZE);
-                    // trim the beginning of the array to retain N snapshots
                     snapshots_stats.drain(0..upper_bound_remove);
-
-                    // print!("Snapshot stats: [");
-                    // for i in snapshots_stats.iter() {
-                    //     print!("{}", i);
-                    // }
-                    // println!("]")
                 }
             }
             Err(e) => {
@@ -107,15 +96,16 @@ fn main() {
         }
         thread::sleep(Duration::from_secs(1));
     });
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         // let snapshot_for_worker_thread = Arc::clone(&snapshots);
         let cloned_counter_2 = Arc::clone(&request_counter);
         pool.execute(move || {
-            handle_connection(stream);
             let mut current_counter = cloned_counter_2.lock().unwrap();
             *current_counter += 1;
+            handle_connection(stream);
         })
     }
 }
@@ -137,8 +127,16 @@ fn handle_connection(mut stream: TcpStream) {
         "HTTP/1.1 404 NOT FOUND"
     };
 
-    let response = format!("{}", status_line,);
+    let contents = String::from("Hey there");
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+    stream.shutdown(std::net::Shutdown::Both).unwrap();
 }
